1b9c10d2558959d74e860eb75c8c25726e9a5b7d
Teachoc <Teachoc@outlook.com>
2019-03-03 20:39:46 +0800

    添加用户登录、注销，虽然还没什么用

---
+++ Bangumi/Models/AccessToken.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bangumi.Models
+{
+    public class AccessToken
+    {
+        public string access_token { get; set; }
+        public int expires_in { get; set; }
+        public string token_type { get; set; }
+        public string scope { get; set; }
+        public string refresh_token { get; set; }
+        public int user_id { get; set; }
+    }
+}
--- Bangumi/Pages/TimeLinePage.xaml.cs
+++ Bangumi/Pages/TimeLinePage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using Bangumi.Models;
+﻿using Bangumi.Facades;
+using Bangumi.Models;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
