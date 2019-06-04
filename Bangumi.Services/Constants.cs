4de41d231de77c57762a0a6d3605d9380d76e7d4
Teachoc <Teachoc@outlook.com>
2019-06-04 16:26:00 +0800

    Update Constants.cs

--- Bangumi.Services/Constants.cs
+++ Bangumi.Services/Constants.cs
@@ -9,9 +9,9 @@ namespace Bangumi.Services
     public static class Constants
     {
         // 将自己申请的应用相关信息填入
-        public const string client_id = "";
-        public const string client_secret = "";
-        public const string redirect_url = "";
+        public const string ClientId = "";
+        public const string ClientSecret = "";
+        public const string RedirectUrl = "";
         public const string NoImageUri = "ms-appx:///Assets/NoImage.png";
 
     }
